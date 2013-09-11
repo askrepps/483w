7c08d14a638c46155a1009a7e08350a7c336bfd3
Andrew Krepps <ask224@psu.edu>
2013-09-11 18:41:37 -0400

    added xcode project

---
+++ ios/ExtremeBeatAnnihilation/ExtremeBeatAnnihilation/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ExtremeBeatAnnihilation
+//
+//  Created by ANDREW STEPHEN KREPPS on 9/11/13.
+//  Copyright Crash Course Gaming 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}

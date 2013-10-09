059c81e54e6801dcd543541a91819a994645609e
Skalhaxor <skalex_989@hotmail.com>
2013-10-09 13:39:26 -0400

    App now displays Exteme Beat Annihilation instead of Hello World

--- android/ExtremeBeatAnnihilation/Classes/HelloWorldScene.cpp
+++ android/ExtremeBeatAnnihilation/Classes/HelloWorldScene.cpp
@@ -51,7 +51,7 @@ bool HelloWorld::init()
 
     // add a label shows "Hello World"
     // create and initialize a label
-    CCLabelTTF* pLabel = CCLabelTTF::create("Hello World", "Thonburi", 34);
+    CCLabelTTF* pLabel = CCLabelTTF::create("Extreme Beat Annihilation", "Thonburi", 34);
 
     // ask director the window size
     CCSize size = CCDirector::sharedDirector()->getWinSize();

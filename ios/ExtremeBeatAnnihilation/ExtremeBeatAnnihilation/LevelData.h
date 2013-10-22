c20535851dc49ec4ad39376eee099c183300b5b4
ANDREW STEPHEN KREPPS <ask224@boucke103-lab-mac214.clc-labs.its.psu.edu>
2013-10-22 17:00:10 -0400

    Added LevelData class for parsing audio files (and to test committing again).

---
+++ ios/ExtremeBeatAnnihilation/ExtremeBeatAnnihilation/LevelData.h
@@ -0,0 +1,15 @@
+//
+//  LevelData.h
+//  ExtremeBeatAnnihilation
+//
+//  Created by ANDREW STEPHEN KREPPS on 10/22/13.
+//  Copyright (c) 2013 Crash Course Gaming. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LevelData : NSObject
+{
+}
+
+@end
---
+++ ios/ExtremeBeatAnnihilation/ExtremeBeatAnnihilation/LevelData.m
@@ -0,0 +1,13 @@
+//
+//  LevelData.m
+//  ExtremeBeatAnnihilation
+//
+//  Created by ANDREW STEPHEN KREPPS on 10/22/13.
+//  Copyright (c) 2013 Crash Course Gaming. All rights reserved.
+//
+
+#import "LevelData.h"
+
+@implementation LevelData
+
+@end

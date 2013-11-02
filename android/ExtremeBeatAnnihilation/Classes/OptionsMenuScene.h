ffe0e32ffa225dc1d95e5f7558fe3b82392ac7cb
wurzyking <blw5180@psu.edu>
2013-11-02 17:43:02 -0400

    Started Options Menu

---
+++ android/ExtremeBeatAnnihilation/Classes/OptionsMenuScene.h
@@ -0,0 +1,22 @@
+#ifndef __OPTIONS_MENU_SCENE_H__
+#define __OPTIONS_MENU_SCENE_H__
+
+#include "cocos2d.h"
+
+class OptionsMenu : public cocos2d::CCLayer
+{
+public:
+    virtual bool init();
+
+    // there's no 'id' for android cocos2d, so we recommend to returning the exact class pointer
+    static cocos2d::CCScene* scene();
+
+    // a selector callback
+    void menuCloseCallback(CCObject* pSender);
+
+    // implement the "static node()" method manually
+    CREATE_FUNC(OptionsMenu);
+};
+
+#endif // __OPTIONS_MENU_SCENE_H__
+
--- android/ExtremeBeatAnnihilation/proj.android/jni/Android.mk
+++ android/ExtremeBeatAnnihilation/proj.android/jni/Android.mk
@@ -8,7 +8,9 @@ LOCAL_MODULE_FILENAME := libgame
 
 LOCAL_SRC_FILES := hellocpp/main.cpp \
                    ../../Classes/AppDelegate.cpp \
-                   ../../Classes/HelloWorldScene.cpp
+                   ../../Classes/HelloWorldScene.cpp \
+                   ../../Classes/OptionsMenuScene.cpp \
+				   ../../Classes/MainMenuScene.cpp 
                    
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../Classes                   
 

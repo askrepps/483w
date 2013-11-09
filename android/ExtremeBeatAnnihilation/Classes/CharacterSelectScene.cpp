5c67076cf436b2813e9b7d78dc96b2dc11438bde
Rykkata <nstuter@hotmail.com>
2013-11-09 16:38:58 -0500

    Implemented the skeleton to CharacterSelectCallBack, made the images bigger

--- android/ExtremeBeatAnnihilation/Classes/CharacterSelectScene.cpp
+++ android/ExtremeBeatAnnihilation/Classes/CharacterSelectScene.cpp
@@ -18,28 +18,49 @@ CCScene* CharacterSelect::scene()
 // on "init" you need to initialize your instance
 bool CharacterSelect::init()
 {
-    CCSize           size;
-    CCMenuItemImage* closeItem;
-    CCLabelTTF*      label;
+    CCSize           size;						// The size of the screen
+    CCMenuItemFont* closeItem;					// Goes back to the previous scene
+    CCLabelTTF*      label;						// The label to display current status
+    CCMenuItemImage* characterOne;				// Character One for the select screen
+    CCMenuItemImage* characterTwo;				// Character Two for the select screen
 
+    // Super init, MUST DO
     if(!CCLayer::init())
     {
         return false;
     }
 
     // Set whether it is single player or not
-    isSinglePlayer = true;
+    m_isSinglePlayer = true;
+
+    // Initialize number of characters to 0
+    m_numCharacters = 0;
 
     // get the window size from the director
     size = CCDirector::sharedDirector()->getWinSize();
 
-    // add a "close" icon to exit the process
-    closeItem = CCMenuItemImage::create("CloseNormal.png", "CloseSelected.png", this,
-                                           menu_selector(CharacterSelect::MenuCloseCallback));
-    closeItem->setPosition( ccp(CCDirector::sharedDirector()->getWinSize().width - 20, 20) );
 
-    // create menu
-    m_characterMenu = CCMenu::create(closeItem, NULL);
+    // add a "close" icon to exit the process
+    closeItem = CCMenuItemFont::create("Back",  this,
+                                           menu_selector(CharacterSelect::MenuCloseCallback));
+    closeItem->setPosition( ccp(CCDirector::sharedDirector()->getWinSize().width - 80, 20) );
+
+
+
+	// Create the character menu items and load them into memory, set their tags
+	characterOne = CCMenuItemImage::create("CharacterIcons/CharacterOne.png", "CharacterIcons/selectCharacterOne.png", this,
+			menu_selector(CharacterSelect::MenuCharacterCallback));
+	characterOne->setTag(1);
+	characterTwo = CCMenuItemImage::create("CharacterIcons/CharacterTwo.png", "CharacterIcons/selectCharacterTwo.png", this,
+			menu_selector(CharacterSelect::MenuCharacterCallback));
+	characterTwo->setTag(2);
+
+	// Set their positions relative to the screen size, I used 8 as it gave the most buffer room, we can add a third easily
+	characterOne->setPosition( ccp(size.width / 8, CCDirector::sharedDirector()->getWinSize().height / 2));
+	characterTwo->setPosition( ccp((size.width / 8) * 4 + 10, CCDirector::sharedDirector()->getWinSize().height / 2));
+
+    // create menu, adding in all the MenuItems
+    m_characterMenu = CCMenu::create(closeItem, characterOne, characterTwo, NULL);
     m_characterMenu->setPosition(CCPointZero);
     this->addChild(m_characterMenu, 1);
 
@@ -51,21 +72,9 @@ bool CharacterSelect::init()
     return true;
 }
 
-void CharacterSelect::LoadCharacters(const char* charDirectory)
+void CharacterSelect::LoadCharacters()
 {
-	CCMenuItemImage* characterOne;				// Character One for the select screen
-	CCMenuItemImage* characterTwo;				// Character Two for the select screen
 
-
-	// Create the character menu items
-	characterOne = CCMenuItemImage::create("CharacterIcons/CharacterOne.png", "CharacterIcons/selectCharacterOne.png", this,
-			menu_selector(CharacterSelect::MenuCharacterCallback));
-	characterOne = CCMenuItemImage::create("CharacterIcons/CharacterTwo.png", "CharacterIcons/selectCharacterTwo.png", this,
-			menu_selector(CharacterSelect::MenuCharacterCallback));
-
-	// Set their positions
-	characterOne->setPosition( ccp(20, CCDirector::sharedDirector()->getWinSize().height - 40));
-	characterTwo->setPosition( ccp(20 + 400, CCDirector::sharedDirector()->getWinSize().height - 40));
 }
 
 // a selector callback
@@ -81,5 +90,19 @@ void CharacterSelect::MenuCloseCallback(CCObject* sender)
 
 void CharacterSelect::MenuCharacterCallback(CCObject* sender)
 {
+	++m_numCharacters;
+	CCMenuItemImage* convert = (CCMenuItemImage *)sender;
+	if(m_numCharacters == 1)
+	{
+		m_selectedCharOne = convert->getTag();
+	}
+	else if(m_numCharacters == 2)
+	{
+		m_selectedCharTwo = convert->getTag();
+	}
 
+	if(m_numCharacters >= 2)
+	{
+
+	}
 }

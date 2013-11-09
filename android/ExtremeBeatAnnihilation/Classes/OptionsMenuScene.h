e17ac2699e0d49782e19fdff5c7e93f817a61598
Rykkata <nstuter@hotmail.com>
2013-11-09 14:48:45 -0500

    Changed Init to init and Scene to scene

--- android/ExtremeBeatAnnihilation/Classes/OptionsMenuScene.h
+++ android/ExtremeBeatAnnihilation/Classes/OptionsMenuScene.h
@@ -7,10 +7,10 @@
 class OptionsMenu : public cocos2d::CCLayer
 {
 public:
-    virtual bool Init();
+    virtual bool init();
 
     // there's no 'id' for android cocos2d, so we recommend to returning the exact class pointer
-    static cocos2d::CCScene* Scene();
+    static cocos2d::CCScene* scene();
 
     // a selector callback
     void MenuCloseCallback(CCObject* pSender);

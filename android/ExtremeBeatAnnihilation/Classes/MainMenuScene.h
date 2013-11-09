edf8834781fc4e99f34d3865993b20b3df9774b7
Skalhaxor <skalex_989@hotmail.com>
2013-11-09 17:15:13 -0500

    Create initial set up of the main menu scene

--- android/ExtremeBeatAnnihilation/Classes/MainMenuScene.h
+++ android/ExtremeBeatAnnihilation/Classes/MainMenuScene.h
@@ -7,8 +7,12 @@ class MainMenu : public cocos2d::CCLayer
 {
 public:
     virtual bool              init();
-    static  cocos2d::CCScene* scene();
-            void              MenuCloseCallback(CCObject* pSender);
+    static  cocos2d::CCScene* Scene();
+
+    void CallbackSelectSinglePlayer(CCObject* sender);
+    void CallbackSelectMultiplayer(CCObject* sender);
+    void CallbackSelectOptions(CCObject* sender);
+    void CallbackSelectExit(CCObject* sender);
 
     // implement the "static node()" method manually
     CREATE_FUNC(MainMenu);

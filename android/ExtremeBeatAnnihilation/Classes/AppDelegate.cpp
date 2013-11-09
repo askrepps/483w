ddd0209de8822e5057e3dae20e0704f20f38f0ed
Skalhaxor <skalex_989@hotmail.com>
2013-11-09 17:15:13 -0500

    Create initial set up of the main menu scene

--- android/ExtremeBeatAnnihilation/Classes/AppDelegate.cpp
+++ android/ExtremeBeatAnnihilation/Classes/AppDelegate.cpp
@@ -33,7 +33,7 @@ bool AppDelegate::applicationDidFinishLaunching()
     pDirector->setAnimationInterval(1.0 / 60);
 
     // create a scene. it's an autorelease object
-    CCScene* pScene = OptionsMenu::scene();
+    CCScene* pScene = MainMenu::Scene();
 
     // run
     pDirector->runWithScene(pScene);

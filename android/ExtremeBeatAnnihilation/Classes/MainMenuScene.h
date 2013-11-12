28bcb2cdcd2ab775fac0907713675b0d03b1b444
Skalhaxor <skalex_989@hotmail.com>
2013-11-12 13:19:59 -0500

    Updated menu font and padding between menu items in main menu

--- android/ExtremeBeatAnnihilation/Classes/MainMenuScene.h
+++ android/ExtremeBeatAnnihilation/Classes/MainMenuScene.h
@@ -7,9 +7,10 @@
 
 #define POSITION_HALF_SCREEN 0.5                 // factor of width/height which give a position halfway
 #define BACKGROUND_IMAGE     "splashscreen.jpg"  // image to display as the background
-#define MENU_COLOR           ccc3(255, 0, 0)     // RGB color of the menu items
+#define MENU_COLOR           ccc3(255, 255, 255) // RGB color of the menu items
 #define MENU_FONT_SIZE       50                  // size of the font for the menu items
-#define MENU_FONT_STYLE      "Thonburi"          // style of the font for the menu items
+#define MENU_FONT_STYLE      "Arial"             // style of the font for the menu items
+#define MENU_ITEM_PADDING    20                  // padding between menu items, most likely in pixels
 
 class MainMenu : public cocos2d::CCLayer
 {

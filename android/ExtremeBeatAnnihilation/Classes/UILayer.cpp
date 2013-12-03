60f21cff2ac2fd1e4095fbe170fef4e47fe4045c
Skalhaxor <skalex_989@hotmail.com>
2013-12-03 12:02:56 -0500

    Add a pause button to the game scene UI

--- android/ExtremeBeatAnnihilation/Classes/UILayer.cpp
+++ android/ExtremeBeatAnnihilation/Classes/UILayer.cpp
@@ -7,16 +7,37 @@ using namespace cocos2d;
 // return - false if there was an error in initializing, true otherwise
 bool UILayer::init()
 {
+    CCLabelTTF*      labelPause;  // the text for pause menu item
+    CCMenuItemLabel* itemPause;   // menu item for opening the pause menu
+    CCMenu*          menu;        // menu to contain the menu item
+    CCSize           size;        // the size of the window
+
     if(!CCLayer::init())
     {
         return false;
     }
 
-    // TODO: remove this as it is temporary to display that this is the main game scene when running
-    CCSize size = CCDirector::sharedDirector()->getWinSize();
-    CCLabelTTF* tempHeader = CCLabelTTF::create("Main Game Scene", "Arial", 50);
-    tempHeader->setPosition( ccp(size.width * 0.5, size.height * 0.9) );
-    this->addChild(tempHeader);
+    // get the window size from the director
+    size = CCDirector::sharedDirector()->getWinSize();
+
+    // create the pause menu item
+    labelPause = CCLabelTTF::create("Pause", MENU_FONT_STYLE, MENU_FONT_SIZE);
+    itemPause  = CCMenuItemLabel::create(labelPause, this, menu_selector(UILayer::HandlePausePressed));
+    itemPause->setColor(MENU_COLOR);
+    itemPause->setPosition(size.width * PAUSE_WIDTH_POS, size.height * PAUSE_HEIGHT_POS);
+
+    // create the menu containing the pause item
+    menu = CCMenu::create(itemPause, NULL);
+    menu->setPosition(CCPointZero);
+    this->addChild(menu, 1);
 
     return true;
+}
+
+// On selecting the pause menu item, pause the game and bring up the pause menu
+//
+// sender [in] - the object that sent the selected event
+void UILayer::HandlePausePressed(CCObject* sender)
+{
+
 }
--- android/ExtremeBeatAnnihilation/Classes/UILayer.h
+++ android/ExtremeBeatAnnihilation/Classes/UILayer.h
@@ -3,10 +3,18 @@
 
 #include "cocos2d.h"
 
+#define PAUSE_WIDTH_POS      0.9                 // factor of screen with to position pause menu item
+#define PAUSE_HEIGHT_POS     0.1                 // factor of screen height to position pause menu item
+#define MENU_COLOR           ccc3(255, 255, 255) // RGB color of the menu items
+#define MENU_FONT_SIZE       50                  // size of the font for the menu items
+#define MENU_FONT_STYLE      "Arial"             // style of the font for the menu items
+
 class UILayer : public cocos2d::CCLayer
 {
 public:
     virtual bool init();
+
+    void HandlePausePressed(CCObject* sender);
 
     // allows use of create() function which uses init() to create this layer
     CREATE_FUNC(UILayer);
